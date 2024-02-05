322eca353af5feb82970ec0fca283c63906458fa
Benton Li <cl2597@cornell.edu>
2024-02-05 21:43:21 +0800

    Fading text

--- app/Choice.tsx
+++ app/Choice.tsx
@@ -17,6 +17,7 @@ export default function Choice() {
     "I know you love me 🥹",
     "I can't live without you! 🥹",
     "Babe that hurts~~ 🥹",
+    "wwwwwwwww~~ 🥹",
   ];
   const [display, setDisplay] = useState(false);
   const onYesClick = () => {
@@ -26,7 +27,8 @@ export default function Choice() {
   const onNoClick = () => {
     setNClick(nClick + 1);
     setYesSize(yesSize + 4);
-    setNoSize(noSize - 2);
+    if (nClick > 4) setNoSize(noSize - 4);
+    else setNoSize(noSize - 1);
   };
   return (
     <div className="flex flex-row text-center lg:mb-10 lg:text-center mb-10">
@@ -44,31 +46,38 @@ export default function Choice() {
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-rose-300 h-screen w-screen z-[10]">
             <div className="flex h-screen items-center justify-center">
               <h2 className={"text-5xl font-semibold"}>
-                <p>Yes Babe! 😘😘😘</p>
+                {nClick < zhananyulu.length ? (
+                  <p>Yes Babe! 😘😘😘</p>
+                ) : (
+                  <p>What a choice Babe! 😘😘😘</p>
+                )}
               </h2>
             </div>
           </div>
         )}
       </div>
-      <div
-        style={{ transform: `translate(0,${-nClick * 20}px)` }}
-        className="ml-3"
-      >
-        <button
-          className={`bg-rose-400 hover:border-rose-300/50 hover:bg-rose-300/50 rounded-lg border border-transparent px-5 py-4 transition-transform hover:translate-y-1 motion-reduce:transform-none`}
-          onClick={onNoClick}
+
+      {nClick < zhananyulu.length && (
+        <div
+          style={{ transform: `translate(0,${-nClick * 15}px)` }}
+          className="ml-3"
         >
-          <h2 className="font-semibold">
-            <p
-              style={{
-                fontSize: noSize + "px",
-              }}
-            >
-              {zhananyulu[nClick]}
-            </p>
-          </h2>
-        </button>
-      </div>
+          <button
+            className={`bg-rose-400 hover:border-rose-300/50 hover:bg-rose-300/50 rounded-lg border border-transparent px-5 py-4 transition-transform hover:translate-y-1 motion-reduce:transform-none`}
+            onClick={onNoClick}
+          >
+            <h2 className="font-semibold">
+              <p
+                style={{
+                  fontSize: noSize + "px",
+                }}
+              >
+                {zhananyulu[nClick]}
+              </p>
+            </h2>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       </div>
       <div className="relative flex flex-col place-items-center before:absolute z-[-1] mb-10">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4">
-          Would you be my valentine? 🌹
+          Will you be my valentine? 🌹
         </p>
       </div>
 
